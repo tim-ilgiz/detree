9cecd25c8ec675f67607d128c0d4f1ed2f21ff8b
Ilgiz Timrukov <tim.ilgiz@gmail.com>
2020-02-03 23:49:51 +0300

    add AuthServer

---
+++ auth/AuthServer.Infrastructure/Data/Identity/PersistedGrantDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace AuthServer.Infrastructure.Data.Identity
+{
+    public class PersistedGrantDbContextFactory : IDesignTimeDbContextFactory<PersistedGrantDbContext>
+    {
+        public PersistedGrantDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<PersistedGrantDbContext>();
+            optionsBuilder.UseNpgsql("host=localhost;port=5432;database=dtdb;username=dtuser;password=dtpass",
+                sql => sql.MigrationsAssembly(typeof(PersistedGrantDbContextFactory).GetTypeInfo().Assembly.GetName().Name));
+            return new PersistedGrantDbContext(optionsBuilder.Options, new OperationalStoreOptions());
+        }
+    }
+}
